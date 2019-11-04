3e5429d1dc28ed6d3e5ad812387c48c8080c2426
Lukasz Brzozko <lukasz9099@gmail.com>
2019-11-04 20:38:58 +0100

    Add styles for classifieds widget

--- server.js
+++ server.js
@@ -1,57 +1,73 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const fetch = require('node-fetch');
-const convert = require('xml-js');
+const path = require("path");
+const fetch = require("node-fetch");
+const convert = require("xml-js");
 
 const port = process.env.PORT || 3030;
 
-app.use('/static', express.static(path.join(__dirname, 'public')))
-app.use(express.json())
+app.use("/static", express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
 // const indexRouter = require(path.join(__dirname, '/routes/index'));
 app.disable("x-powered-by");
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname + '/public' })
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname + "/public" });
 });
-app.get('/ogloszenia', (req, res) => {
+app.get("/ogloszenia", (req, res) => {
+  const postsCount = 4;
 
-    const postsCount = 4;
-
-    const sendResponse = () => {
-        const url = 'URL TO XML';
-        fetch(url)
-            .then(response => response.text())
-            .then(str => convert.xml2json(str))
-            .then(data => JSON.parse(data))
-            .then(data => data.elements[0].elements[0].elements.filter(el => el.name === 'item').splice(0, postsCount))
-            .then(arr => arr.map(el => el.elements))
-            .then(arr => {
-                const data = [];
-                arr.forEach(arr2 => data.push(
-                    arr2.filter(el => {
-                        if (el.name === 'title' || el.name === 'content:encoded' || el.name === 'pubDate') { return el }
-                    })))
-                return { ...data };
+  const sendResponse = () => {
+    const url = "URL TO XML";
+    fetch(url)
+      .then(response => response.text())
+      .then(str => convert.xml2json(str))
+      .then(data => JSON.parse(data))
+      .then(data =>
+        data.elements[0].elements[0].elements
+          .filter(el => el.name === "item")
+          .splice(0, postsCount)
+      )
+      .then(arr => arr.map(el => el.elements))
+      .then(arr => {
+        const data = [];
+        arr.forEach(arr2 =>
+          data.push(
+            arr2.filter(el => {
+              if (
+                el.name === "title" ||
+                el.name === "content:encoded" ||
+                el.name === "pubDate"
+              ) {
+                return el;
+              }
             })
-            .then(dataObject => {
-                const posts = [];
-                for (const key in dataObject) {
-                    const title = dataObject[key].find(el => el.name === 'title').elements[0].text;
-                    const content = dataObject[key].find(el => el.name === 'content:encoded').elements[0].cdata;
-                    const pubDate = dataObject[key].find(el => el.name === 'pubDate').elements[0].text;
-                    posts.push({
-                        title,
-                        content,
-                        pubDate
-                    })
-                }
-                return posts
-            })
-            .then(json => res.json(json))
-    }
-    sendResponse()
+          )
+        );
+        return { ...data };
+      })
+      .then(dataObject => {
+        const posts = [];
+        for (const key in dataObject) {
+          const title = dataObject[key].find(el => el.name === "title")
+            .elements[0].text;
+          const content = dataObject[key].find(
+            el => el.name === "content:encoded"
+          ).elements[0].cdata;
+          const pubDate = dataObject[key].find(el => el.name === "pubDate")
+            .elements[0].text;
+          posts.push({
+            title,
+            content,
+            pubDate
+          });
+        }
+        return posts;
+      })
+      .then(json => res.json(json));
+  };
+  sendResponse();
 });
 // app.get('*', (req, res) => {
 //     res.redirect('/')
@@ -59,6 +75,6 @@ app.get('/ogloszenia', (req, res) => {
 
 // app.use('/', indexRouter);
 
-
-
-app.listen(port, () => console.log(`Server is listening on http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Server is listening on http://localhost:${port}`)
+);
--- src/components/ClassifiedItem/ClassifiedItem.js
+++ src/components/ClassifiedItem/ClassifiedItem.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import './ClassifiedItem.scss';
+import React from "react";
+import "./ClassifiedItem.scss";
 const ClassifiedItem = props => {
+  const { title, content, pubDate } = props.children;
+  const localDate = new Date(pubDate).toLocaleString();
 
-    const { title, content, pubDate } = props.children;
-    const localDate = new Date(pubDate).toLocaleString();
-
-    return (
-
-        <li className='classifieds__item'>
-            <article className="article">
-                <div className="article__title-wrapper" onClick={e => { e.currentTarget.nextElementSibling.classList.toggle('article__content-container--active') }}>
-                    <h1 className="article__title">{title}</h1>
-                    <span className="article__show">+</span>
-                </div>
-                <div className="article__content-container">
-                    <p className="article__published">{localDate}</p>
-                    <div className="article__content"
-                        dangerouslySetInnerHTML={{ __html: content }}>
-                    </div>
-                </div>
-            </article>
-        </li >
-
-    );
-}
+  return (
+    <li className="classifieds__item">
+      <article className="article">
+        <div
+          className="article__title-wrapper"
+          onClick={e => {
+            e.currentTarget.nextElementSibling.classList.toggle(
+              "article__content-container--active"
+            );
+          }}
+        >
+          <h1 className="article__title">{title}</h1>
+          <span className="article__show">+</span>
+        </div>
+        <div className="article__content-container">
+          <p className="article__published">Dodano: {localDate}</p>
+          <div
+            className="article__content"
+            dangerouslySetInnerHTML={{ __html: content }}
+          ></div>
+        </div>
+      </article>
+    </li>
+  );
+};
 
 export default ClassifiedItem;
