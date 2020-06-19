1829b2c0d6ed94a2e69f3ff64b389d1bbd3e374f
Lukasz Brzozko <lukasz9099@gmail.com>
2020-06-19 15:18:55 +0200

    Remove comments

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -4,20 +4,9 @@ import ScheduleWidget from "../ScheduleWidget/ScheduleWidget";
 import ClassifiedsWidget from "../ClassifiedsWidget/ClassifiedsWidget";
 import { Link } from "react-router-dom";
 
-const HomeView = props => {
-  // createAllTimetableVariants = () => {
-  //     generateTimetable(timetableWeeks.FIRSTWEEK, 'first-week')
-  //     generateTimetable(timetableWeeks.SECONDWEEK, 'second-week')
-  //     generateTimetable(timetableWeeks.THIRDWEEK, 'third-week')
-  // }
-
+const HomeView = (props) => {
   return (
     <>
-      {/* <header className="topbar">
-        <div className="topbar__container">
-          <p className="topbar__date">Data</p>
-        </div>
-      </header> */}
       <div className="header__link-container">
         <Link to="/login" className="header__login-link">
           Zaloguj
